1e5dcb91159c2041f5e1778ee4345829a9330fc8
heheheejin <e_jaxx@hotmail.com>
2022-09-23 01:06:44 +0800

    Update navbar - redirect to home page onclick logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,17 +4,19 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-light">
             <div className="container-fluid px-5 w-75">
-                <span className="navbar-brand h1 mr-auto">AxPhone</span>
+                <span className="navbar-brand h1 mr-auto">
+                    <a
+                        href="/"
+                        className="fw-bold text-decoration-none text-reset"
+                    >
+                        AxPhone
+                    </a>
+                </span>
 
                 <ul className="nav navbar-nav d-flex">
                     <li className="nav-item">
-                        <a className="nav-link active" href="/">
-                            Contacts
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/create-contact">
-                            Add
+                        <a className="nav-link active" href="/create-contact">
+                            Create New Contact
                         </a>
                     </li>
                 </ul>
