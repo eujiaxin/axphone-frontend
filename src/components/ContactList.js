cd99f240423cef3cf020e0236ad375e8ae66c8df
heheheejin <e_jaxx@hotmail.com>
2022-09-23 03:19:46 +0800

    Create modal for edit form

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import API from "../utils/API";
+import Modal from "./Modal";
 
 const ContactList = () => {
     const [contacts, setContacts] = useState([]);
@@ -31,48 +32,56 @@ const ContactList = () => {
     };
 
     return (
-        <div className="col-md-6 col-sm-10 mx-auto p-0 space-between">
-            <ul className="list-group p-5">
-                {contacts.length === 0 ? (
-                    <span className="text-center">
-                        There are no contacts at the moment. Add new contacts
-                        now!
-                    </span>
-                ) : (
-                    contacts.map((contact) => (
-                        <li className="list-group-item p-3 d-flex justify-content-between">
-                            <div className="ms-2 me-auto">
-                                <div className="h2">{contact.name}</div>
-                                <span className="h5">
-                                    {contact.phone_number}
-                                </span>
-                            </div>
-                            <div className="btn-group-vertical" role="group">
-                                <button
-                                    class="btn btn-outline-dark col-6"
-                                    type="button"
-                                    data-toggle="tooltip"
-                                    data-placement="top"
-                                    title="Edit"
+        <>
+            <Modal />
+            <div className="col-md-6 col-sm-10 mx-auto p-0 space-between">
+                <ul className="list-group p-5">
+                    {contacts.length === 0 ? (
+                        <span className="text-center">
+                            There are no contacts at the moment. Add new
+                            contacts now!
+                        </span>
+                    ) : (
+                        contacts.map((contact) => (
+                            <li className="list-group-item p-3 d-flex justify-content-between">
+                                <div className="ms-2 me-auto">
+                                    <div className="h2">{contact.name}</div>
+                                    <span className="h5">
+                                        {contact.phone_number}
+                                    </span>
+                                </div>
+                                <div
+                                    className="btn-group-vertical"
+                                    role="group"
                                 >
-                                    Edit
-                                </button>
-                                <button
-                                    class="btn btn-outline-danger col-6"
-                                    type="button"
-                                    data-toggle="tooltip"
-                                    data-placement="top"
-                                    title="Delete"
-                                    onClick={() => deleteHandler(contact.id)}
-                                >
-                                    Delete
-                                </button>
-                            </div>
-                        </li>
-                    ))
-                )}
-            </ul>
-        </div>
+                                    <button
+                                        type="button"
+                                        className="btn btn-outline-dark col-6"
+                                        data-bs-toggle="modal"
+                                        data-bs-target="#editModal"
+                                    >
+                                        Edit
+                                    </button>
+
+                                    <button
+                                        class="btn btn-outline-danger col-6"
+                                        type="button"
+                                        data-toggle="tooltip"
+                                        data-placement="top"
+                                        title="Delete"
+                                        onClick={() =>
+                                            deleteHandler(contact.id)
+                                        }
+                                    >
+                                        Delete
+                                    </button>
+                                </div>
+                            </li>
+                        ))
+                    )}
+                </ul>
+            </div>
+        </>
     );
 };
 
---
+++ src/components/Modal.js
@@ -0,0 +1,35 @@
+const Modal = () => {
+    return (
+        <div class="modal fade" id="editModal" tabindex="-1">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="editModalLabel">
+                            Edit Contact
+                        </h5>
+                        <button
+                            type="button"
+                            class="btn-close"
+                            data-bs-dismiss="modal"
+                        ></button>
+                    </div>
+                    <div class="modal-body">Form here</div>
+                    <div class="modal-footer">
+                        <button
+                            type="button"
+                            class="btn btn-secondary"
+                            data-bs-dismiss="modal"
+                        >
+                            Close
+                        </button>
+                        <button type="button" class="btn btn-primary">
+                            Save changes
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Modal;
