5238798396a323a19d3e0705a09c06ed4d169758
heheheejin <e_jaxx@hotmail.com>
2022-09-23 02:43:38 +0800

    Fix request header typo

--- src/utils/API.js
+++ src/utils/API.js
@@ -4,6 +4,6 @@ export default axios.create({
     baseURL: "http://127.0.0.1:8000/api/",
     headers: {
         Accept: "application/json",
-        "Content-Type": "application.json",
+        "Content-Type": "application/json",
     },
 });
