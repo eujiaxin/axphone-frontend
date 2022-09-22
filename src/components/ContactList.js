0f580e0c56ccc10d417fc58e808f63ab705ff7e3
heheheejin <e_jaxx@hotmail.com>
2022-09-22 23:22:31 +0800

    Add edit button for contacts

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -8,12 +8,30 @@ const ContactList = () => {
     return (
         <div className="col-md-6 col-sm-10 mx-auto p-0 space-between">
             <ul className="list-group p-5">
-                {contacts.map((contact) => (
-                    <li className="list-group-item p-3">
-                        <h2>{contact.name}</h2>
-                        <span>{contact.phoneNumber}</span>
-                    </li>
-                ))}
+                {contacts.length === 0 ? (
+                    <span className="text-center">
+                        There are no contacts at the moment. Add new contacts
+                        now!
+                    </span>
+                ) : (
+                    contacts.map((contact) => (
+                        <li className="list-group-item p-3 d-flex justify-content-between">
+                            <div className="ms-2 me-auto">
+                                <h2>{contact.name}</h2>
+                                <span>{contact.phoneNumber}</span>
+                            </div>
+                            <button
+                                class="btn btn-outline-dark"
+                                type="button"
+                                data-toggle="tooltip"
+                                data-placement="top"
+                                title="Edit"
+                            >
+                                Edit
+                            </button>
+                        </li>
+                    ))
+                )}
             </ul>
         </div>
     );
