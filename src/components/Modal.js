1bb2125eb4bac79494d8ed4e75aa4aa40f832d96
heheheejin <e_jaxx@hotmail.com>
2022-09-23 03:42:09 +0800

    Create edit contact modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,8 @@
-const Modal = () => {
+import API from "../utils/API";
+
+const Modal = (props) => {
     return (
-        <div class="modal fade" id="editModal" tabindex="-1">
+        <div class="modal fade" id="editModal" tabIndex="-1">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
@@ -13,7 +15,40 @@ const Modal = () => {
                             data-bs-dismiss="modal"
                         ></button>
                     </div>
-                    <div class="modal-body">Form here</div>
+                    <div class="modal-body">
+                        <form>
+                            <div class="mb-3 px-3">
+                                <label for="name" class="col-form-label">
+                                    Name:
+                                </label>
+                                <input
+                                    type="text"
+                                    class="form-control"
+                                    id="name"
+                                    placeholder={props.originalName}
+                                    onChange={(e) =>
+                                        props.setName(e.target.value)
+                                    }
+                                />
+                            </div>
+                            <div class="mb-3 px-3">
+                                <label
+                                    for="phone_number"
+                                    class="col-form-label"
+                                >
+                                    Phone Number:
+                                </label>
+                                <input
+                                    class="form-control"
+                                    id="phone_number"
+                                    placeholder={props.originalPhoneNumber}
+                                    onChange={(e) =>
+                                        props.setPhoneNumber(e.target.value)
+                                    }
+                                />
+                            </div>
+                        </form>
+                    </div>
                     <div class="modal-footer">
                         <button
                             type="button"
@@ -22,7 +57,14 @@ const Modal = () => {
                         >
                             Close
                         </button>
-                        <button type="button" class="btn btn-primary">
+                        <button
+                            type="button"
+                            class="btn btn-primary"
+                            data-bs-dismiss="modal"
+                            onClick={() => {
+                                props.editHandler(props.contactId);
+                            }}
+                        >
                             Save changes
                         </button>
                     </div>
